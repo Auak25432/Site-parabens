a945c8d3ad6c715d8c428f92535a613f3820ffff
Auak25432 <kauabarboza764@gmail.com>
2025-06-08 13:15:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
   acao: "🔥 Adrenalina, luta e superação!",
   
   // Características
-  menina_de_deus: "🙏 Uma jovem guiada pela fé, com um coração voltado a Deus e aos bons valores.",
+  meninaDeDeus: "🙏 Uma jovem guiada pela fé, com um coração voltado a Deus e aos bons valores.",
   gentil: "💖 Uma pessoa que trata os outros com carinho, atenção e respeito.",
   inteligente: "🧠 Raciocínio rápido, sabedoria e uma mente curiosa e brilhante.",
   linda: "🌟 Beleza por fora e ainda mais por dentro, com um brilho único.",
